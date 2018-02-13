27e33ec7447d1c368d6f9da2de783750c333f697
Storm Van Soldt <stormvansoldt@yahoo.com>
2018-02-12 19:05:02 -0700

    added argparse and updated formatting on sexy_hex output

--- PyDump/pydump.py
+++ PyDump/pydump.py
@@ -3,25 +3,31 @@
 import sys
 import socket
 import threading
+import argparse
 
 # TODO: Implement the sexy_hex function
 
 def sexy_hex(in_bytes):
 	'''
-	Take an input string (from either end of the data stream) and turn it 
+	Take an input bytes object (from either end of the data stream) and turn it 
 	into a sexy output string showing the starting value, the hex value for
 	each character, and the character string represented by the hex value.
+
+	Returns a tuple where the first element is the entire sexified hex output,
+	and the second element is the length of the input string.
 	'''
 	output_str	= ''
 	LENGTH 		= 16
 	in_str		= in_bytes.decode('UTF-8').strip('\n')
+	output_len	= len(in_str)
 	chunks 		= [in_str[i:i+LENGTH] for i in range(0, len(in_str), LENGTH)]
 
 	for index, chunk in enumerate(chunks):
-		hex_str = ' '.join(['{:02X}'.format(ord(x)) for x in chunk])
+		hex_str 	= ' '.join(['{:02X}'.format(ord(x)) for x in chunk])
 		output_str += ('{0:#0{1}x}  {2:<50}{3}\n'.format(index, 4, hex_str, chunk))
 
-	return(output_str)
+	output_str 	= output_str.rstrip('\n')
+	return((output_str, output_len))
 
 def local_handler(l_sock, r_sock):
 	'''
@@ -32,9 +38,10 @@ def local_handler(l_sock, r_sock):
 		data = l_sock.recv(1024)
 		if not data:
 			break
-		print('[^] Recieved X bytes from localhost\n{}'.format(sexy_hex(data)))
+		sexy_out, data_len = sexy_hex(data)
+		print('[ ^ ] Recieved {0} bytes from localhost\n{1}'.format(data_len, sexy_out))
 		r_sock.sendall(data)
-		print('[<--] Sent X bytes to remote host')
+		print('[<--] Sent {} bytes to remote host\n'.format(data_len))
 
 def remote_handler(l_sock, r_sock):
 	'''
@@ -45,9 +52,10 @@ def remote_handler(l_sock, r_sock):
 		data = r_sock.recv(1024)
 		if not data:
 			break
-		print('[^] Recieved X bytes from remote\n{}'.format(sexy_hex(data)))
+		sexy_out, data_len = sexy_hex(data)
+		print('[ ^ ] Recieved {0} bytes from remote\n{1}'.format(data_len, sexy_out))
 		l_sock.sendall(data)
-		print('[-->] Sent X bytes to localhost')
+		print('[-->] Sent {} bytes to localhost\n'.format(data_len))
 
 def proxy_handler():
 	'''
@@ -59,15 +67,27 @@ def proxy_handler():
 	remote_handler and local_handler to output from both ends of the pipe.
 
 	Using the try:catch block waits for one/both of the threads using 
-	thread.join(). If shit goes back, 
+	thread.join(). Since the threads are daemonized, we ensure both are
+	killed when the program closes.
 	'''
-	rhost		= '127.0.0.1'
-	lhost		= '127.0.0.1'
-	rport		= 8001
-	lport 		= 9001
-	sock		= socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-	sock.bind((lhost, lport))
+	parser = argparse.ArgumentParser(
+		   description='TCP proxy written in Python because Python is awesome.')
+	parser.add_argument('l_port',
+						type=int,
+						metavar='PORT',
+						help='local port to bind the listener to')
+	parser.add_argument('target',
+						type=str,
+						metavar='TARGET',
+						help='remote host to connect to in the form of (IP:PORT)')
+
+	args 			= parser.parse_args()
+	r_host, r_port	= tuple(args.target.split(':'))
+	proxy_addr		= ('127.0.0.1', args.l_port)
+	sock			= socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+	sock.bind(proxy_addr)
 	sock.listen(1)
 	
 	print('[*] Waiting for local connection...')
@@ -75,13 +95,13 @@ def proxy_handler():
 
 	print('[*] Received connection from {0!s}:{1!s}'.format(l_addr[0], l_addr[1]))
 	
-	print('[*] Connecting to rhost {0!s}:{1!s}...'.format(rhost, rport))
-	r_conn		= socket.create_connection((rhost, rport))
+	print('[*] Connecting to {0!s}:{1!s}...'.format(r_host, r_port))
+	r_conn		= socket.create_connection((r_host, r_port))
 
 	print('-----------------------------------------')
-	print('| {0!s}:{1!s} =====> {2!s}:{3!s} |'.format(l_addr[0], l_addr[1], rhost, rport))
+	print('| {0!s}:{1!s} =====> {2!s}:{3!s} |'.format(l_addr[0], l_addr[1], r_host, r_port))
 	print('| Connection established!               |')
-	print('-----------------------------------------')
+	print('-----------------------------------------\n')
 
 	t_remote	= threading.Thread(target=remote_handler, args=[l_conn, r_conn], daemon=True)
 	t_local		= threading.Thread(target=local_handler, args=[l_conn, r_conn], daemon=True)
