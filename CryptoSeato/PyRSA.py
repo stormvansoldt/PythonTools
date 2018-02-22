b4fb35641592a264e105fe6e9da4cecec00816a6
Storm Van Soldt <stormvansoldt@yahoo.com>
2018-02-22 02:16:20 -0700

    added import_key to create an RSA key object from the bytestring paramaters in rsa_encrypt and rsa_decrypt

--- CryptoSeato/PyRSA.py
+++ CryptoSeato/PyRSA.py
@@ -7,29 +7,11 @@ from Crypto.Random import get_random_bytes
 
 def generate_keypair():
 	'''
-	This function takes a user-defined input as a password, then uses it to
-	generate a 4096-bit public and private RSA keypair. These keys are then 
-	saved to ~/.pykeys/ in the user's home directory as "private.key" and
-	"public.key".
+	This function generates a new 2048-bit RSA keypair, and stores the public
+	and private halves in pub_key and priv_key, respectively.
 
-	Returns a tuple containing the keypair in the form (pub_key, priv_key)
+	Returns the keypair as a tuple.
 	'''
-	path			= os.path.expanduser('~/') + '.pykeys/'
-
-	if os.path.isfile(path + 'private.key'):
-		print('[!] WARNING: it looks like a private and/or public key already exists. If you continue, those keys will be overwritten!')
-		print('[!] Are you sure you want to continue? (y/n)')
-		answer = input().lower()
-
-		if answer == 'n':
-			print('[*] Don\'t worry, your keys are safe!')
-			return 0
-		elif answer == 'y':
-			print('[*] Overwriting old key files...')
-		else:
-			print('[!] Unknown input. Shutting down, notifying Skynet')
-			return 1
-
 	print('[*] Generating RSA keypair...')
 	
 	key 		 	= RSA.generate(2048)
@@ -37,57 +19,36 @@ def generate_keypair():
 	priv_key		= key.exportKey(pkcs=8,
 									protection='scryptAndAES128-CBC')
 
-	if not os.path.isdir(path):
-		print('[*] Creating \'~/.pykeys/\' directory...')
-		os.makedirs(path)
-
-	with open(path + 'private.key', 'wb') as privfile:
-		print('[*] Exporting private key...')
-		privfile.write(priv_key)
-
-	with open(path + 'public.key', 'wb') as pubfile:
-		print('[*] Exporting public key...')
-		pubfile.write(pub_key)
-
 	print('[*] RSA keys successfully generated!')
-	print('[*] They have been stored in {}'.format(path))
 
 	return (pub_key, priv_key)
 
 
-def rsa_encrypt(key):
+def rsa_encrypt(msg, pub_key):
 	'''
-	Generate an AES-128 key to be used as the symmetric encryption key for the
-	remainder of the communication. We then encrypt this key with the public 
-	RSA key (which will be sent to us by the client), and return that encrypted
-	session key.
+	Takes a msg (intended to be the raw 128-bit AES key, but can really be any
+	data) as the first argument and encrypts it with the public RSA key passed
+	as the second argument.
+
+	Returns the encrypted message.
 	'''
-	path		= os.path.expanduser('~/') + '.pykeys/'
-
-	with open(path + 'public.key', 'rb') as keyfile:
-		pub_key = RSA.importKey(keyfile.read())
-
+	pub_key 	= RSA.import_key(pub_key)
 	rsa_cipher 	= PKCS1_OAEP.new(pub_key)
 
-	return rsa_cipher.encrypt(key)
+	return rsa_cipher.encrypt(msg)
 
 
-def rsa_decrypt(enc_key):
+def rsa_decrypt(enc_msg, priv_key):
 	'''
-	Imports the private key, prompts for the passphrase, then uses the key
-	to decrypt the content of the data argument. Returns the decrypted plain-
-	text.
+	Takes an encrypted message and decrypts it using the private key passed
+	as the second argument. 
 
-	THIS WILL BE RUN ON THE CLIENT.
+	Returns the decrypted message.
 	'''
-	path			= os.path.expanduser('~/') + '.pykeys/'
+	priv_key 	= RSA.import_key(priv_key)
+	rsa_cipher 	= PKCS1_OAEP.new(priv_key)
 
-	with open(path + 'private.key', 'rb') as keyfile:
-		priv_key = RSA.importKey(keyfile.read())
-
-	rsa_cipher = PKCS1_OAEP.new(priv_key)
-
-	return rsa_cipher.decrypt(enc_key)
+	return rsa_cipher.decrypt(enc_msg)
 
 
 #generate_keypair()
