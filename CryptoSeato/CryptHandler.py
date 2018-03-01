afc549e396136f313606e176a5b51f3d4a177558
Storm Van Soldt <stormvansoldt@yahoo.com>
2018-02-28 23:33:42 -0700

    updated files to conform to PEP8

--- CryptoSeato/CryptHandler.py
+++ CryptoSeato/CryptHandler.py
@@ -7,103 +7,106 @@ from Crypto.Random import get_random_bytes
 
 '''
 TODO:
- - Test with an external module (probably PyCat or something like that) 
+ - Test with an external module (probably PyCat or something like that)
 
 This module has the functions needed to handle the initialization of the
 encryption libray. Here is a simple walkthrough of the init process:
 
  1. Ensure a valid connection between the server and client.
  2. After connection is validated, the client creates the RSA keypair and
- 	sends the public key to the server.
+    sends the public key to the server.
  3. The server accepts the public key from the client.
  4. The server generates a random sequence of 16 bytes to be used as the
- 	AES encryption key for the remainder of the session.
+    AES encryption key for the remainder of the session.
  5. Server encrypts the session key using the client's public RSA key
  6. Encrypted session key is sent back to the client.
  7. Client decrypts the AES key using the private RSA key.
  8. Both ends of the connection now have the session key, and the encrypted
- 	communication can begin.
+    communication can begin.
 
 '''
 
+
 def server_handler(conn):
-	'''
-	Receive an RSA public key from the client, generate a 128-bit AES session
-	key, encrypt it with the public key, then send it back to the client.
+    '''
+    Receive an RSA public key from the client, generate a 128-bit AES session
+    key, encrypt it with the public key, then send it back to the client.
 
-	Keyword arguments:
-	conn -- socket object representing the connection to the client.
+    Keyword arguments:
+    conn -- socket object representing the connection to the client.
 
-	Returns:
-	 session_key -- bytestring of the AES key
-	'''
-	if not conn:
-		print('[!] Error! Invalid connection was passed')
-		return 1
+    Returns:
+     session_key -- bytestring of the AES key
+    '''
+    if not conn:
+        print('[!] Error! Invalid connection was passed')
+        return 1
 
-	print('[ ] Waiting to receive client\'s public key...')
-	pub_key 	= conn.recv(2048)
-	print('[*] Received public key!')
+    print('[ ] Waiting to receive client\'s public key...')
+    pub_key = conn.recv(2048)
+    print('[*] Received public key!')
 
-	print('[ ] Generating AES session key and encrypting it with the public RSA key...')
-	session_key	= get_random_bytes(16)
+    print('[ ] Generating AES session key and encrypting it with the public '
+          'RSA key...')
+    session_key = get_random_bytes(16)
 
-	print('[*] Sending AES key to client')
-	conn.sendall(PyRSA.rsa_encrypt(session_key, pub_key))
+    print('[*] Sending AES key to client')
+    conn.sendall(PyRSA.rsa_encrypt(session_key, pub_key))
 
-	return session_key
+    return session_key
 
 
 def client_handler(conn):
-	'''
-	Generate a 2048-bit RSA keypair, send the public key to the server,
-	receive the encrypted AES session key as a response, then decrypt it
-	using the private RSA key.
+    '''
+    Generate a 2048-bit RSA keypair, send the public key to the server,
+    receive the encrypted AES session key as a response, then decrypt it
+    using the private RSA key.
 
-	Keyword arguments:
-	 conn -- socket object representing the connection to the server.
+    Keyword arguments:
+     conn -- socket object representing the connection to the server.
 
-	Returns:
-	 session_key -- bytestring of the AES key
-	'''
-	if not conn:
-		print('[!] Error! Invalid connection was provided')
-		return 1
+    Returns:
+     session_key -- bytestring of the AES key
+    '''
+    if not conn:
+        print('[!] Error! Invalid connection was provided')
+        return 1
 
-	(pub_key, priv_key) = PyRSA.generate_keypair()
-	
-	print('[ ] Sending public key to the server...')
-	conn.sendall(pub_key)
+    (pub_key, priv_key) = PyRSA.generate_keypair()
 
-	print('[ ] Waiting to receive the encrypted session key...')
-	enc_session_key 	= conn.recv(2048)
+    print('[ ] Sending public key to the server...')
+    conn.sendall(pub_key)
 
-	print('[*] Received encrypted session key!')
-	session_key 		= PyRSA.rsa_decrypt(enc_session_key, priv_key)
+    print('[ ] Waiting to receive the encrypted session key...')
+    enc_session_key = conn.recv(2048)
 
-	print('[*] Decrypted the session key!')
+    print('[*] Received encrypted session key!')
+    session_key = PyRSA.rsa_decrypt(enc_session_key, priv_key)
 
-	return session_key
+    print('[*] Decrypted the session key!')
+
+    return session_key
 
 
 def main():
-	arg = sys.argv[1]
+    arg = sys.argv[1]
 
-	if arg == 'l':
-		sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-		sock.bind(('0.0.0.0', 32123))
-		sock.listen(1)
-		conn, addr = sock.accept()
-		print(server_handler(conn))
-		conn.close()
-		sock.close()
+    if arg == 'l':
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.bind(('0.0.0.0', 32123))
+        sock.listen(1)
+        conn, addr = sock.accept()
+        print(server_handler(conn))
+        conn.close()
+        sock.close()
 
-	elif arg == 'c':
-		conn = socket.create_connection(('127.0.0.1', 32123))
-		print(client_handler(conn))
-		conn.close()
-	else:
-		print('try again asshole')
+    elif arg == 'c':
+        conn = socket.create_connection(('127.0.0.1', 32123))
+        print(client_handler(conn))
+        conn.close()
+    else:
+        print('try again asshole')
+
 
 if __name__ == '__main__':
-	main()
+    main()
--- CryptoSeato/PyRSA.py
+++ CryptoSeato/PyRSA.py
@@ -1,51 +1,49 @@
 #!/usr/bin/env python
 
-import os
 from Crypto.PublicKey import RSA
 from Crypto.Cipher import PKCS1_OAEP
-from Crypto.Random import get_random_bytes
+
 
 def generate_keypair():
-	'''
-	This function generates a new 2048-bit RSA keypair, and stores the public
-	and private halves in pub_key and priv_key, respectively.
+    '''
+    This function generates a new 2048-bit RSA keypair, and stores the public
+    and private halves in pub_key and priv_key, respectively.
 
-	Returns the keypair as a tuple.
-	'''
-	print('[*] Generating RSA keypair...')
-	
-	key 		 	= RSA.generate(2048)
-	pub_key			= key.publickey().exportKey()
-	priv_key		= key.exportKey(pkcs=8,
-									protection='scryptAndAES128-CBC')
+    Returns the keypair as a tuple.
+    '''
+    print('[*] Generating RSA keypair...')
 
-	print('[*] RSA keys successfully generated!')
+    key = RSA.generate(2048)
+    pub_key = key.publickey().exportKey()
+    priv_key = key.exportKey(pkcs=8, protection='scryptAndAES128-CBC')
 
-	return (pub_key, priv_key)
+    print('[*] RSA keys successfully generated!')
+
+    return (pub_key, priv_key)
 
 
 def rsa_encrypt(msg, pub_key):
-	'''
-	Takes a msg (intended to be the raw 128-bit AES key, but can really be any
-	data) as the first argument and encrypts it with the public RSA key passed
-	as the second argument.
+    '''
+    Takes a msg (intended to be the raw 128-bit AES key, but can really be any
+    data) as the first argument and encrypts it with the public RSA key passed
+    as the second argument.
 
-	Returns the encrypted message.
-	'''
-	pub_key 	= RSA.import_key(pub_key)
-	rsa_cipher 	= PKCS1_OAEP.new(pub_key)
+    Returns the encrypted message.
+    '''
+    pub_key = RSA.import_key(pub_key)
+    rsa_cipher = PKCS1_OAEP.new(pub_key)
 
-	return rsa_cipher.encrypt(msg)
+    return rsa_cipher.encrypt(msg)
 
 
 def rsa_decrypt(enc_msg, priv_key):
-	'''
-	Takes an encrypted message and decrypts it using the private key passed
-	as the second argument. 
+    '''
+    Takes an encrypted message and decrypts it using the private key passed
+    as the second argument.
 
-	Returns the decrypted message.
-	'''
-	priv_key 	= RSA.import_key(priv_key)
-	rsa_cipher 	= PKCS1_OAEP.new(priv_key)
+    Returns the decrypted message.
+    '''
+    priv_key = RSA.import_key(priv_key)
+    rsa_cipher = PKCS1_OAEP.new(priv_key)
 
-	return rsa_cipher.decrypt(enc_msg)
+    return rsa_cipher.decrypt(enc_msg)
